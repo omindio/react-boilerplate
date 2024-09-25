00f18cb9e84e51c299e6c865acc1660b94d8ef62
omind.io <48690665+omindio@users.noreply.github.com>
2024-09-26 01:32:52 +0200

    Added NotFoundPage and UnauthorizedPage

--- src/domains/auth/pages/UnauthorizedPage.tsx
+++ src/domains/auth/pages/UnauthorizedPage.tsx
@@ -1,11 +1,24 @@
 import React, { useEffect } from 'react';
+import { Button, Result } from 'antd';
+import { Link } from 'react-router-dom';
 
 const UnauthorizedPage: React.FC = () => {
   useEffect(() => {
     document.title = 'Unauthorized';
   }, []);
 
-  return <>UnauthorizedPage</>;
+  return (
+    <Result
+      status="403"
+      title="403"
+      subTitle="Lo sentimos, no tienes acceso a esta página."
+      extra={
+        <Button type="primary">
+          <Link to="/dashboard">Volver al Dashboard</Link>
+        </Button>
+      }
+    />
+  );
 };
 
 export default UnauthorizedPage;
--- src/shared/pages/NotFoundPage.tsx
+++ src/shared/pages/NotFoundPage.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Button, Result } from 'antd';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage: React.FC = () => {
+  useEffect(() => {
+    document.title = '404';
+  }, []);
+
   return (
-    <div>
-      <h1>404 - Página no encontrada</h1>
-      <p>Lo sentimos, la página que estás buscando no existe.</p>
-    </div>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Lo sentimos, la página que visitaste no existe."
+      extra={
+        <Button type="primary">
+          <Link to="/dashboard">Volver al Dashboard</Link>
+        </Button>
+      }
+    />
   );
 };
 
